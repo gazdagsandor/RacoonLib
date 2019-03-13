8e414a4149a90ab475a88bd92649316738a901d3
Sándor Gazdag <sandor.gazdag@iov42.com>
2019-03-13 14:54:11 +0100

    Initial Commit

---
+++ RacoonLib/RacoonLib.h
@@ -0,0 +1,19 @@
+//
+//  RacoonLib.h
+//  RacoonLib
+//
+//  Created by Gazdag Sándor on 2019. 03. 13..
+//  Copyright © 2019. Gazdag Sándor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RacoonLib.
+FOUNDATION_EXPORT double RacoonLibVersionNumber;
+
+//! Project version string for RacoonLib.
+FOUNDATION_EXPORT const unsigned char RacoonLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RacoonLib/PublicHeader.h>
+
+
